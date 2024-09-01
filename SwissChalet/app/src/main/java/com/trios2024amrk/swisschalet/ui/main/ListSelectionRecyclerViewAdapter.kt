1060913b155fa2a55fec5c48a3e550cbfd5e7b9a
RezaKhibari <160886458+RezaKhibari@users.noreply.github.com>
2024-08-31 23:59:41 -0400

    End of ch 8

--- SwissChalet/app/src/main/java/com/trios2024amrk/swisschalet/ui/main/ListSelectionRecyclerViewAdapter.kt
+++ SwissChalet/app/src/main/java/com/trios2024amrk/swisschalet/ui/main/ListSelectionRecyclerViewAdapter.kt
@@ -3,12 +3,13 @@ package com.trios2024amrk.swisschalet.ui.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.trios2024amrk.swisschalet.TaskList
 import com.trios2024amrk.swisschalet.databinding.ListSelectionViewHolderBinding
 
-class ListSelectionRecyclerViewAdapter :
-    RecyclerView.Adapter<ListSelectionViewHolder>()
+class ListSelectionRecyclerViewAdapter(private val lists :
+    MutableList<TaskList>): RecyclerView.Adapter<ListSelectionViewHolder>()
 {
-     val listTitles = arrayOf("Title1", "Title2")
+     //val listTitles = arrayOf("Title1", "Title2")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder
     {
@@ -19,13 +20,17 @@ class ListSelectionRecyclerViewAdapter :
 
     override fun getItemCount(): Int
     {
-        return listTitles.size
+        return lists.size
     }
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int)
     {
         holder.binding.itemNumber.text = (position + 1).toString()
-        holder.binding.itemString.text = listTitles[position]
+        holder.binding.itemString.text = lists[position].name
+    }
+
+    fun listsUpdated() {
+        notifyItemInserted(lists.size-1)
     }
 
 }
--- SwissChalet/app/src/main/java/com/trios2024amrk/swisschalet/ui/main/MenuFragment.kt
+++ SwissChalet/app/src/main/java/com/trios2024amrk/swisschalet/ui/main/MenuFragment.kt
@@ -6,33 +6,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.trios2024amrk.swisschalet.R
 import com.trios2024amrk.swisschalet.databinding.FragmentMainBinding
 
-class MenuFragment : Fragment() {
+class MenuFragment : Fragment()
+{
 
     private lateinit var binding: FragmentMainBinding
-    companion object {
+    private lateinit var viewModel: MainViewModel
+
+    companion object
+    {
         fun newInstance() = MenuFragment()
     }
 
-    private val viewModel: MainViewModel by viewModels()
+    override fun onActivityCreated(savedInstanceState: Bundle?)
+    {
+        super.onActivityCreated(savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+        this.viewModel = ViewModelProvider(requireActivity(),
+            MainViewModelFactory(PreferenceManager.getDefaultSharedPreferences(requireActivity())))
+            .get(MainViewModel::class.java)
+
+        val recyclerViewAdapter = ListSelectionRecyclerViewAdapter(viewModel.lists)
+
+        binding.listsRecyclerview.adapter = recyclerViewAdapter
+
+        viewModel.onListAdded = {
+            recyclerViewAdapter.listsUpdated()
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
 
         // TODO: Use the ViewModel
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View
+    {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         binding.listsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
-        binding.listsRecyclerview.adapter = ListSelectionRecyclerViewAdapter()
+        //binding.listsRecyclerview.adapter = ListSelectionRecyclerViewAdapter()
         return binding.root
     }
 
